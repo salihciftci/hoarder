05f1bd5846bde531eef58c8dcb7828dc6707146c
Salih Çiftçi <salih@ciftci.co>
2019-07-09 22:47:48 +0300

    removed express

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-var express = require('express');
-var http = require("http").createServer(app)
-var path = require('path');
-var logger = require('morgan');
+var http = require("http")
 var io = require("socket.io")(http)
-
-var indexRouter = require('./routes/index');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
 
 var game = false
 playerCount = 0
@@ -89,9 +74,7 @@ io.on("connection", (socket) => {
             player2.score = client.data.gold
         }
         io.emit("update", { player1, player2, "game": game })
-        console.log(player1, player2);
     })
 })
 
-http.listen(5002, () => { console.log("Live:3000") })
-app.listen(5001)
+io.listen(3000, () => { console.log("Live:3000") })
