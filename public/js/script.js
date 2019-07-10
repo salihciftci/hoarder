eb1d8595dd0e9da795cde553f680eb829fb145e9
Salih Çiftçi <salih@ciftci.co>
2019-07-10 22:55:32 +0300

    made changes because of server side

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 class Player {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
 
-        this.gold = 0
+        this.gold = 0;
 
-        this.image = new Image()
+        this.image = new Image();
         this.image.src = "../img/stop.png"
     }
 
@@ -24,12 +24,12 @@ class Player {
 
 class Gold {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
 
 
-        this.image = new Image()
-        this.image.src = "../img/coin/1.png"
+        this.image = new Image();
+        this.image.src = "../img/coin/1.png";
         this.src = [
             "../img/coin/1.png",
             "../img/coin/2.png",
@@ -47,13 +47,13 @@ class Gold {
             "../img/coin/14.png",
             "../img/coin/15.png",
             "../img/coin/16.png",
-        ]
+        ];
 
         this.count = 0
     }
 
     loop() {
-        this.count++
+        this.count++;
         if (this.count === 15) {
             this.count = 0
         }
@@ -67,10 +67,10 @@ class Gold {
 
 class Game {
     constructor() {
-        this.canvas = document.getElementById("canvas")
-        this.canvas.focus()
-        this.ctx = this.canvas.getContext("2d")
-        this.timer = null
+        this.canvas = document.getElementById("canvas");
+        this.canvas.focus();
+        this.ctx = this.canvas.getContext("2d");
+        this.timer = null;
 
         this.socket = io("localhost:3000")
     }
@@ -79,84 +79,86 @@ class Game {
 
         // Giving random position to player
         while (true) {
-            var x = Math.floor(Math.random() * 800)
+            var x = Math.floor(Math.random() * 800);
             if (x % 20 === 0) {
                 break;
             }
         }
 
         while (true) {
-            var y = Math.floor(Math.random() * 800)
+            var y = Math.floor(Math.random() * 800);
             if (y % 20 === 0) {
                 break;
             }
         }
 
-        this.ctx.font = "20px Arial"
-        this.ctx.fillText("Diğer Oyuncu Bekleniyor..", 270, 400)
+        this.ctx.font = "20px Arial";
+        this.ctx.fillText("Diğer Oyuncu Bekleniyor..", 270, 400);
 
-        this.player = new Player(x, y)
+        this.player = new Player(x, y);
 
         // Telling socket to I'm Ready
-        this.socket.emit("init", { "id": this.socket.id, "data": this.player })
+        this.socket.emit("init", {"id": this.socket.id, "data": this.player});
 
         this.socket.on("init", (server) => {
             if (server.game) {
-                this.player.draw(this.ctx)
-                this.player2 = new Player(server.player2.x, server.player2.y)
-                this.gold = new Gold(600, 360)
-                this.gold.draw(this.ctx)
-                this.ctx.drawImage(this.player2.image, this.player2.x, this.player2.y, 64, 64)
-                console.log(this.player, this.player2);
+                this.player.draw(this.ctx);
+                this.player2 = new Player(server.players[1].x, server.players[1].y);
+                this.gold = new Gold(600, 360);
+                this.gold.draw(this.ctx);
+                this.ctx.drawImage(this.player2.image, this.player2.x, this.player2.y, 64, 64);
                 game.start()
             } else {
-                // clearInterval(this.timer)
-                clearTimeout(this.timer)
-                this.ctx.clearRect(0, 0, 800, 800)
-                this.ctx.font = "20px Arial"
+                clearInterval(this.timer);
+                // clearTimeout(this.timer)
+                this.ctx.clearRect(0, 0, 800, 800);
+                this.ctx.font = "20px Arial";
                 this.ctx.fillText("Diğer Oyuncu Bekleniyor..", 270, 400)
             }
-        })
+        });
 
         this.socket.on("update", (players) => {
-            this.player2.x = players.player2.x
-            this.player2.y = players.player2.y
-        })
+            this.player2.x = players.players[1].x;
+            this.player2.y = players.players[1].y;
+            console.log(players)
+        });
     }
 
     start() {
-        this.canvas.addEventListener("keydown", this.move.bind(this), false)
-        this.canvas.addEventListener("keyup", () => { this.player.stop() }, false)
+        this.canvas.addEventListener("keydown", this.move.bind(this), false);
+        this.canvas.addEventListener("keyup", () => {
+            this.player.stop()
+        }, false);
 
         this.timer = setInterval(this.update.bind(this), 100)
     }
 
     draw() {
-        this.socket.emit("update", { "id": this.socket.id, "data": this.player })
-        this.gold.loop()
+        this.socket.emit("update", {"id": this.socket.id, "data": this.player});
+        this.gold.loop();
 
-        this.ctx.clearRect(0, 0, 800, 800)
-        this.ctx.drawImage(this.gold.image, this.gold.x, this.gold.y, 24, 24)
+        this.ctx.clearRect(0, 0, 800, 800);
+        this.ctx.drawImage(this.gold.image, this.gold.x, this.gold.y, 24, 24);
 
-        this.player.draw(this.ctx)
-        this.player2.draw(this.ctx)
+        this.player.draw(this.ctx);
+        this.player2.draw(this.ctx);
 
-        this.ctx.font = "20px Arial"
+        this.ctx.font = "20px Arial";
         this.ctx.fillText("Score: " + this.player.gold.toString(), 5, 795)
     }
 
     update() {
         if (this.player.x + 32 === this.gold.x + 12 && this.player.y + 32 === this.gold.y + 12) {
-            this.player.gold++
+            this.player.gold++;
 
             while (true) {
-                this.gold.x = Math.floor(Math.random() * 800)
+                this.gold.x = Math.floor(Math.random() * 800);
                 if (this.gold.x % 20 === 0) {
                     break;
                 }
             }
             while (true) {
-                this.gold.y = Math.floor(Math.random() * 800)
+                this.gold.y = Math.floor(Math.random() * 800);
                 if (this.gold.y % 20 === 0) {
                     break;
                 }
@@ -187,14 +189,15 @@ class Game {
                     this.player.x += 20
                 }
                 break;
-            default: break;
+            default:
+                break;
         }
 
-        this.player.move()
+        this.player.move();
         this.draw()
     }
 }
 
 
-var game = new Game()
-game.init()
+const game = new Game();
+game.init();
